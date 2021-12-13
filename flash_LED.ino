9b228bc14268ab6791c5fbf5c01d1f9b66b75916
dash13ll <dc3863@nyu.edu>
2021-12-13 18:12:05 -0500

    Update flash_LED.ino
    Added another led and had it flash opposite from the first LED

--- flash_LED.ino
+++ flash_LED.ino
@@ -1,11 +1,15 @@
 int flashLedNow = 33;
+int flashMob = 34;
 void setup() {
   pinMode(flashLedNow, OUTPUT);
+  pinMode(flashMob, OUTPUT);
 }
 
 void loop() {
   digitalWrite(flashLedNow, HIGH);
+  digitalWrite(flashMob, LOW);
   delay(500);
-  digitalWrite(flashLedNow, LOW);
+  digitalWrite(flashLedNo, LOW);
+  digitalWrite(flashMob, HIGH);
   delay(500);
 }
